9672a937d88d2f8b2d3822868fce47d9bae0af23
Ryosuke Uchida <uchiryo7@gmail.com>
2017-04-08 16:37:10 +0900

    Add event model

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,24 @@
+class EventsController < ApplicationController
+  def index
+    events = Event.new
+  end
+
+  def show
+
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'events', to: 'events#index'
+
+  get 'events', to: 'events#show'
+
+  get 'events', to: 'events#new'
+
+  post 'events', to: 'events#create'
+
+  get 'events', to: 'events#edit'
+
+  patch 'events', to: 'events#update'
+
+  delete 'events', to: 'events#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

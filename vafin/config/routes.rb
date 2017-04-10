f7b5037b590316f977df382b26d1c7d87f917d6d
Ryosuke Uchida <uchiryo7@gmail.com>
2017-04-11 01:55:27 +0900

    Add controllers and create routes. Create events conroller and users controller

--- vafin/config/routes.rb
+++ vafin/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  post 'events', to: 'events#create'
+
+  get 'events/:id', to: 'events#show'
+
+  get 'events/:id/edit', to: 'events#edit'
+
+  get 'events/new', to: 'events#new'
+
+  get 'users/new', to: 'users#new'
+
+  post 'users', to: 'users#create'
+
+  get 'users/:id', to: 'users#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

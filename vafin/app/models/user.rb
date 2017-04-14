1ca7e67992184987f1b5c9cd6096e4839bc4d98a
Ryosuke Uchida <uchiryo7@gmail.com>
2017-04-14 20:47:32 +0900

    user edited

--- vafin/app/models/user.rb
+++ vafin/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   belongs_to :event
   # belongs_to :admin
+  has_many :admins, through: :event,
   has_many :follows_from, class_name: Follow, foreign_key: :from_user_id
   has_many :follows_to, class_name: Follow, foreign_key: :to_user_id
   has_many :following, through: :follows_from, source: :to_user
